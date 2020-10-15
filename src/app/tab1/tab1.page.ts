cc8f735faaa5459c7af8893c4b447357b3f198b6
Bruno <alves.bruno945@gmail.com>
2020-10-15 11:19:00 -0300

    Adicionando mais frases motivacionais

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,9 +10,14 @@ export class Tab1Page {
   constructor() {}
 
   funcButton() {
-    const arr = ["Voce é a melhor pessoa","Acredite nos seus sonhos","Voce é incrivel!!!"];
+    const arr = ["Voce é a melhor pessoa",
+    "Acredite nos seus sonhos",
+    "Voce é incrivel","A persistência é o caminho do êxito.", 
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "Imagine uma nova história para sua vida e acredite nela.",
+  "O sucesso é ir de fracasso em fracasso sem perder entusiasmo"];
 
-    const index = Math.floor(Math.random() * 3);  
+    const index = Math.floor(Math.random() * 7);  
 
     document.getElementById("frase").innerText = arr[index]
   }
