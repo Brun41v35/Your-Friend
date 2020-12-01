c441d6bd9a7dd24c60d940b31d78be397ecc5cb2
kielkow <matheuskiel@fiorifer.com.br>
2020-12-01 19:48:02 -0300

    Add new phrases and titles

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,14 +10,21 @@ export class Tab1Page {
   constructor() {}
 
   funcButton() {
-    const arr = ["Voce é a melhor pessoa",
-    "Acredite nos seus sonhos",
-    "Voce é incrivel","A persistência é o caminho do êxito.", 
-    "No meio da dificuldade encontra-se a oportunidade.",
-    "Imagine uma nova história para sua vida e acredite nela.",
-  "O sucesso é ir de fracasso em fracasso sem perder entusiasmo"];
+    const arr = [
+      "Você é a melhor pessoa!",
+      "Acredite nos seus sonhos!",
+      "Você é incrivel!",
+      "A persistência é o caminho do êxito", 
+      "No meio da dificuldade encontra-se a oportunidade",
+      "Imagine uma nova história para sua vida e acredite nela",
+      "O sucesso é ir de fracasso em fracasso sem perder entusiasmo",
+      "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance",
+      "Só existe um êxito: a capacidade de levar a vida que se quer",
+      "A coragem não é ausência do medo; é a persistência apesar do medo",
+      "Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos"
+    ];
 
-    const index = Math.floor(Math.random() * 7);  
+    const index = Math.floor(Math.random() * 11);  
 
     document.getElementById("frase").innerText = arr[index]
   }
