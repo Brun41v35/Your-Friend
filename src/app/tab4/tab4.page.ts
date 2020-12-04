ec2a305e61d32d38bd7eb95b037b6d7ca9ffebd5
Bruno <alves.bruno945@gmail.com>
2020-12-04 17:46:48 -0300

    Adicionando botao fab na tela de anotacoes

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab4',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router,) {}
 
   ngOnInit() {}
+
+  goToNote() {
+    // this.router.navigate(['register'])
+    alert("Yes, i am working!")
+  }
 
 }
