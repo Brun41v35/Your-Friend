5bc4225af22c5c55b9455aab4cd7285c0e3bdfbf
kielkow <matheuskiel@fiorifer.com.br>
2020-12-09 13:49:45 -0300

    request to dicas api

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor() {
+    this.renderDicas()
+  }
 
+  async renderDicas() {
+    const { data } = await axios.get('https://aps-api.gateway.linkapi.com.br/v1/dicas');
+    const { dicas } = data;
+
+    let items = '';
+    for(const dica of dicas) {
+      items = items.concat(
+        `<ion-item>
+          <ion-label>
+            <h2><b>${dica.id}°</b></h2>
+            <h3>${dica.title}</h3>
+            <p>${dica.description}</p>
+          </ion-label>
+        </ion-item>`
+      )
+    }
+
+    document.getElementById("list").innerHTML = items;
+  }
 }
